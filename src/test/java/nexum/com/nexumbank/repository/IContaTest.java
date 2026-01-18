e3234335d0800d31bf41089c7d0a5efa30de62ab
Gabriel Leal <gabriel-o-carinha@hotmail.com>
2026-01-17 21:09:40 -0300

    feat: add some repository tests

--- src/test/java/nexum/com/nexumbank/repository/IContaTest.java
+++ src/test/java/nexum/com/nexumbank/repository/IContaTest.java
@@ -6,8 +6,7 @@ import nexum.com.nexumbank.model.Cliente;
 import nexum.com.nexumbank.model.Conta;
 import nexum.com.nexumbank.model.Usuario;
 import nexum.com.nexumbank.model.enums.StatusConta;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,29 +24,50 @@ class IContaTest {
     @Autowired
     private EntityManager entityManager;
 
+    UsuarioRequestDTO usuarioRequestDTO;
+    Usuario usuario;
+    Cliente cliente;
+
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Executing method before all tests in IContaTest class");
+    }
+
+    @BeforeEach
+    void init() {
+        usuarioRequestDTO = new UsuarioRequestDTO("Gabriel Leal", "123.456.789-00", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
+        usuario = createUsuario(usuarioRequestDTO);
+        cliente = createCliente(usuario);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Executing method after each test in IContaTest class");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Executing method after all tests in IContaTest class");
+    }
+
     @Test
     @DisplayName("Should return true when account number exists")
     void existsByNumeroContaCase1() {
 
-        UsuarioRequestDTO usuarioDTO = new UsuarioRequestDTO("Gabriel Leal", "123.456.789-00", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
-        Usuario usuario = createUsuario(usuarioDTO);
-        Cliente cliente = createCliente(usuario);
+        //Arrange
         Conta conta = createConta(cliente);
 
+        //Act
         Boolean foundedAccount = this.iConta.existsByNumeroConta(conta.getNumeroConta());
 
+        //Assert
         assertThat(foundedAccount).isTrue();
-
     }
 
     @Test
     @DisplayName("Should return false when account number not exists")
     void existsByNumeroContaCase2() {
-
-        UsuarioRequestDTO usuarioDTO = new UsuarioRequestDTO("Gabriel Leal", "123.456.789-00", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
-        Usuario usuario = createUsuario(usuarioDTO);
-        Cliente cliente = createCliente(usuario);
-        Conta conta = createConta(cliente);
 
         String inexistentNumber = "654321";
 
@@ -55,6 +75,23 @@ class IContaTest {
 
         assertThat(foundedAccount).isFalse();
 
+    }
+
+    //TODO ajustar teste para verificar exceção de número duplicado
+    @Test
+    @DisplayName("Should throw exception when trying to create account with duplicate number")
+    void dontCreateContaWithDuplicateNumber(){
+
+        //Arrange
+        Conta conta = createConta(cliente);
+        Conta contaDuplicate = new Conta();
+        contaDuplicate.setNumeroConta(conta.getNumeroConta());
+
+        //Act
+        Conta foundedAccount = this.iConta.findById(conta.getIdConta()).orElseThrow();
+
+        //Assert
+        assertThat(foundedAccount.getNumeroConta()).isNotEqualTo(contaDuplicate.getNumeroConta());
     }
 
     private Usuario createUsuario(UsuarioRequestDTO usuarioRequestDTO) {
--- src/test/java/nexum/com/nexumbank/service/UsuarioServiceTest.java
+++ src/test/java/nexum/com/nexumbank/service/UsuarioServiceTest.java
@@ -3,8 +3,10 @@ package nexum.com.nexumbank.service;
 import jakarta.persistence.EntityManager;
 import nexum.com.nexumbank.dto.usuario.UsuarioRequestDTO;
 import nexum.com.nexumbank.dto.usuario.UsuarioResponseDTO;
+import nexum.com.nexumbank.model.Usuario;
 import nexum.com.nexumbank.repository.IUsuario;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,12 +17,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Optional;
-
+import static org.assertj.core.api.Assertions.anyOf;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
-@DataJpaTest
 @ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class UsuarioServiceTest {
@@ -34,36 +37,76 @@ class UsuarioServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
     @InjectMocks
     private UsuarioService usuarioService;
 
-    @Autowired
-    private EntityManager entityManager;
-
-//    @BeforeEach
-//    void setup(){
-//        MockitoAnnotations.initMocks(this);
-//    }
 
     @Test
     @DisplayName("Should create a new user successfully")
     void criarUsuarioCase1() {
-
         //Arrange
-        UsuarioRequestDTO request = new UsuarioRequestDTO("Gabriel Leal", "123.456.789-00", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
+        Usuario usuario = new Usuario();
+//        usuario.setIdUsuario(1L);
+//        when(repository.save(any())).thenReturn(usuario);
+        when(repository.save(any())).thenAnswer(invocation -> {
+           Usuario u = invocation.getArgument(0);
+           u.setIdUsuario(1L);
+           return u;
+        });
+
+        when(passwordEncoder.encode(any())).thenReturn("senhaCriptografada");
+        UsuarioRequestDTO request = new UsuarioRequestDTO("Gabriel Leal", "094.157.194-78", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
+
         //Act
-        UsuarioResponseDTO response = usuarioService.criarUsuario(request);
+        var output = usuarioService.criarUsuario(request);
+
         //Assert
-        assertNotNull(response);
-        assertNotNull(response.id_usuario());
-        assertEquals("Gabriel Leal", response.nome());
-        assertEquals("123.456.789-00", response.cpf_cnpj());
-        
+        assertNotNull(output);
+        verify(repository).save(any());
     }
 
     @Test
-    @DisplayName("Should throw Exception when a user has not created")
+    @DisplayName("Should don't create a new user if CPF or Email already exists")
     void criarUsuarioCase2() {
+
+        //Arrange
+        Usuario usuario = new Usuario();
+        when(repository.save(any())).thenAnswer(invocation -> {
+            Usuario u = invocation.getArgument(0);
+            u.setIdUsuario(1L);
+            return u;
+        });
+        usuario.setCpfCnpj("094.157.194-78");
+        usuario.setEmail("gabriel@email.com");
+
+        when(passwordEncoder.encode(any())).thenReturn("senhaCriptografada");
+        UsuarioRequestDTO request = new UsuarioRequestDTO("Gabriel Leal", "094.157.194-78", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
+
+        //Act
+        var output = usuarioService.criarUsuario(request);
+
+        //Assert
+        assertEquals(output.cpf_cnpj(), usuario.getCpfCnpj());
+        assertEquals(output.email(), usuario.getEmail());
     }
+
+
+//    @Test
+//    @DisplayName("Should create a new user successfully")
+//    void criarUsuarioCase1() {
+//
+//        //Arrange
+//        UsuarioRequestDTO request = new UsuarioRequestDTO("Gabriel Leal", "094.157.194-78", "gabriel@email.com", "senha123", "11999999999", "Rua A, 123", "SP", "CLIENTE", "07/11/2004", "Programador", 5.000);
+//        //Act
+//        UsuarioResponseDTO response = usuarioService.criarUsuario(request);
+//        entityManager.persist(response);
+//        //Assert
+//        assertThat(response.cpf_cnpj()).isNotEmpty();
+//
+//        //Arrange
+//        //Act
+//        //Assert
+//
+//    }
+
 }
